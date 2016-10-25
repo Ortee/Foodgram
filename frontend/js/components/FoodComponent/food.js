b61ebefbbd23a1fa6208911e499d311cfd850ee1
Mateusz <kontakt@mateuszoracz.pl>
2016-10-25 11:28:17 +0200

    some foodcomponent refactore

--- frontend/js/components/FoodComponent/food.js
+++ frontend/js/components/FoodComponent/food.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Button, Col, Container } from 'reactstrap';
+import { Col } from 'reactstrap';
 
 class Food extends Component {
   constructor(props) {
@@ -58,7 +58,11 @@ class Food extends Component {
           </span>
         </Col>
         <Col className="food-photo">
-          <img className="img-thumbnail" height="600px" width="538px" src="http://dfep0xlbws1ys.cloudfront.net/thumbs2d/dd/2ddd2a4753463c2f396777f0c85502e2.jpg"></img>
+          <img  className="img-thumbnail"
+                height="600px"
+                width="538px"
+                src="http://dfep0xlbws1ys.cloudfront.net/thumbs2d/dd/2ddd2a4753463c2f396777f0c85502e2.jpg">
+          </img>
         </Col>
         <Col className="food-desciption">
           <p className="food-desciption-likes">#awesome #burger #tasty #foods</p>
@@ -66,18 +70,29 @@ class Food extends Component {
         </Col>
         <Col className="food-like">
           <hr/>
-          <span className="food-like-text" onMouseEnter={this.heartOnEnter.bind()} onMouseLeave={this.heartOnLeave.bind()}>83</span>
-          <i aria-hidden="true" className={this.state.heart} onMouseEnter={this.heartOnEnter.bind()} onMouseLeave={this.heartOnLeave.bind()}/>
-
-          <i aria-hidden="true" className={this.state.frown} onMouseEnter={this.faceOnEnter.bind()} onMouseLeave={this.faceOnLeave.bind()}/>
-          <span className="food-unlike-text" onMouseEnter={this.faceOnEnter.bind()} onMouseLeave={this.faceOnLeave.bind()}>13</span>
-
+          <span className="food-like-text"
+                onMouseEnter={this.heartOnEnter.bind()}
+                onMouseLeave={this.heartOnLeave.bind()}>
+                83
+          </span>
+          <i  aria-hidden="true"
+              className={this.state.heart}
+              onMouseEnter={this.heartOnEnter.bind()}
+              onMouseLeave={this.heartOnLeave.bind()}/>
+          <i  aria-hidden="true"
+              className={this.state.frown}
+              onMouseEnter={this.faceOnEnter.bind()}
+              onMouseLeave={this.faceOnLeave.bind()}/>
+          <span className="food-unlike-text"
+                onMouseEnter={this.faceOnEnter.bind()}
+                onMouseLeave={this.faceOnLeave.bind()}>
+                13
+          </span>
           <i  className={this.state.trash}
               onMouseEnter={this.trashOnEnter.bind()}
               onMouseLeave={this.trashOnLeave.bind()}
               aria-hidden="true"
-              onClick={this.props.removePost.bind(null, this.props.post.name, this.props.i)}
-          />
+              onClick={this.props.removePost.bind(null, this.props.post.name, this.props.i)}/>
         </Col>
       </Col>
     );
