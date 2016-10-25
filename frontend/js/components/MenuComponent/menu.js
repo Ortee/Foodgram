b810f91609816c7cd61fe932d903633872c0f060
deniso95 <denis.olek@gmail.com>
2016-10-25 03:20:15 +0200

    deleted unnecesary import from reactstrap

--- frontend/js/components/MenuComponent/menu.js
+++ frontend/js/components/MenuComponent/menu.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
-import { Nav, Col, Navbar, NavbarBrand, NavItem, NavLink } from 'reactstrap';
+import { Nav, Navbar, NavbarBrand, NavItem, NavLink } from 'reactstrap';
 import './menu.scss';
 
 class Menu extends Component {
