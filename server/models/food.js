3ab522dfec0b591816584cca35e836b0ef38553b
Mateusz <kontakt@mateuszoracz.pl>
2016-10-25 18:35:10 +0200

    unfinished requests to server and database /api/FOODS

---
+++ server/models/food.js
@@ -0,0 +1,18 @@
+'use strict';
+module.exports = function(sequelize, DataTypes) {
+  var Food = sequelize.define('Food', {
+    user: DataTypes.STRING,
+    description: DataTypes.TEXT,
+    hashtags: DataTypes.STRING,
+    photo: DataTypes.STRING,
+    likes: DataTypes.INTEGER,
+    dislikes: DataTypes.INTEGER
+  }, {
+    classMethods: {
+      associate: function(models) {
+        // associations can be defined here
+      }
+    }
+  });
+  return Food;
+};
