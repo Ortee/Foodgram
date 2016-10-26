a320ccc2686213c30ca5535dc5e47e092c18d23a
Mateusz <kontakt@mateuszoracz.pl>
2016-10-26 13:58:57 +0200

    add Food seeder & new command: npm run db:seed:undo:all

--- server/seeders/20161014172043-post.js
+++ server/seeders/20161014172043-post.js
@@ -2,14 +2,30 @@
 
 module.exports = {
   up: function (queryInterface, Sequelize) {
-      return queryInterface.bulkInsert('Posts', [{
-        name: 'Test Post from database',
-        content: 'Post content from postgres',
-        created_at: '2016-10-14T20:31:40.000Z',
-        updated_at: '2016-10-14T20:31:40.000Z'
-      }], {});
+      return queryInterface.bulkInsert('Food', [
+        {
+          username: 'AwesomeBurgers',
+          description:'Very tasty',
+          hashtags:'#awesome #burger #tasty #love',
+          photo:'http://dfep0xlbws1ys.cloudfront.net/thumbs2d/dd/2ddd2a4753463c2f396777f0c85502e2.jpg',
+          likes:83,
+          dislikes:13,
+          created_at:'2016-10-14T20:31:40.000Z',
+          updated_at:'2016-10-14T20:31:40.000Z'
+        },
+        {
+          username: 'NiceBurgers',
+          description:'Very tasty',
+          hashtags:'#nice #burger #tasty #love',
+          photo:'http://dfep0xlbws1ys.cloudfront.net/thumbs2d/dd/2ddd2a4753463c2f396777f0c85502e2.jpg',
+          likes:53,
+          dislikes:23,
+          created_at:'2016-10-14T20:31:40.000Z',
+          updated_at:'2016-10-14T20:31:40.000Z'
+        }
+    ], {});
   },
   down: function (queryInterface, Sequelize) {
-      return queryInterface.bulkDelete('Posts', null, {});
+      return queryInterface.bulkDelete('Food', null, {});
   }
 };
