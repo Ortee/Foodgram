9915860ea2fc4b6b25b324f3d68479cd7e599095
Mateusz <kontakt@mateuszoracz.pl>
2016-10-31 18:23:18 +0100

    add form to register component

--- frontend/js/components/RegisterComponent/register.js
+++ frontend/js/components/RegisterComponent/register.js
@@ -15,15 +15,17 @@ class Register extends Component {
         <Col xs={{ size: 10, offset: 1}} id="form">
           <span>Sign up to add photos and comment from your favourite restaurant.</span>
           <hr />
-          <Label hidden>Email</Label>
-          <input type="email" ref="email" placeholder="Email" className="form-control input"/>
-          <Label hidden>Restaurant Name</Label>
-          <input type="text" ref="Username" placeholder="Username" className="form-control input"/>
-          <Label hidden>Login</Label>
-          <input type="text" ref="login" placeholder="Login" className="form-control input"/>
-          <Label hidden>Password</Label>
-          <input type="password" ref="password" placeholder="Password" className="form-control input"/>
-          <Button type="submit" id="button">Sign up</Button>
+          <form ref="registerForm" className="form-inline">
+            <Label hidden>Email</Label>
+            <input type="email" ref="email" placeholder="Email" className="form-control input"/>
+            <Label hidden>Restaurant Name</Label>
+            <input type="text" ref="Username" placeholder="Username" className="form-control input"/>
+            <Label hidden>Login</Label>
+            <input type="text" ref="login" placeholder="Login" className="form-control input"/>
+            <Label hidden>Password</Label>
+            <input type="password" ref="password" placeholder="Password" className="form-control input"/>
+            <Button type="submit" id="button">Sign up</Button>
+          </form>
           <Col id="rules">
             By signing up, you agree to our Terms & Privacy Policy.
           </Col>
