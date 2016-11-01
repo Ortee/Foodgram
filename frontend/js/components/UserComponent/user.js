0f6404f0dd589096c3528fff472f46c62780a6a2
Mateusz <kontakt@mateuszoracz.pl>
2016-11-01 18:44:31 +0100

    userComponent fix mobile version

--- frontend/js/components/UserComponent/user.js
+++ frontend/js/components/UserComponent/user.js
@@ -12,33 +12,37 @@ class User extends Component {
     console.log(this.props.params.name);
     return (
       <section className="user">
-        <Col xs={{ size: 10, offset: 1}}>
+        <Col xs={{ size: 10, offset: 1}} id="information">
           <Col xs={{ size: 4}} id="logo">
           <img  className="img-thumbnail img-logo"
                 height="150px"
                 width="150px"
-                src="http://prezibase.com/wp-content/uploads/2016/02/food-clock-time-diet-meal-eating-health-fitness-prezi-templates.jpg" />
+                src="http://prezibase.com/wp-content/uploads/2016/02/food-clock-time-diet-meal-eating-health-fitness-prezi-templates.jpg"/>
           </Col>
           <Col xs={{ size: 8}}>
-            <Col xs={{ size: 12}}>
+            <Col>
               <span className="username">AwesomeBurgers</span>
             </Col>
-            <Col xs={{ size: 12}}>
-              <span className="info">Likes: 50</span>
+            <Col>
+            <i className="fa fa-heart-o fa-lg" aria-hidden="true"/><span className="info">50</span>
+            <i className="fa fa-frown-o fa-lg" aria-hidden="true"/><span className="info">20</span>
             </Col>
-            <Col xs={{ size: 12}}>
+            <Col className="description">
               Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
             </Col>
           </Col>
         </Col>
+        <Col xs={{size: 10, offset: 1}} id="line">
+          <hr/>
+        </Col>
         <Col xs={{ size: 10, offset: 1}} id="images">
-          { [1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map( (number, index) => {
+          { [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map( (number, index) => {
             return (
               <Col xs={{ size: 4}} key={index} className="image">
                 <img  className="img-thumbnail"
-                      height="300px"
-                      width="300px"
-                      src="https://static1.squarespace.com/static/55355f2fe4b0bb1e28857a4c/56991220fb36b1a2d2c69b9a/56991220a976afc919db1f65/1452872520958/MealPlans.png?format=300w" />
+                      height="100%"
+                      width="100%"
+                      src="https://static1.squarespace.com/static/55355f2fe4b0bb1e28857a4c/56991220fb36b1a2d2c69b9a/56991220a976afc919db1f65/1452872520958/MealPlans.png?format=300w"/>
               </Col>
             );
           })}
