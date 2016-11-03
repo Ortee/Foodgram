a2c48ac7ae5e210b31cbca91f8f361291e30ae18
Mateusz <kontakt@mateuszoracz.pl>
2016-11-03 17:16:31 +0100

    remove hidden labels

--- frontend/js/components/RegisterComponent/register.js
+++ frontend/js/components/RegisterComponent/register.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Col, Label, Button } from 'reactstrap';
+import { Col, Button } from 'reactstrap';
 import './register.scss';
 
 class Register extends Component {
@@ -17,13 +17,9 @@ class Register extends Component {
           <span>Sign up to add photos and comment from your favourite restaurant.</span>
           <hr />
           <form ref="registerForm" onSubmit={this.handleSubmit} className="form-inline">
-            <Label hidden>Email</Label>
             <input type="email" ref="email" placeholder="Email" className="form-control auth-input"/>
-            <Label hidden>Restaurant Name</Label>
             <input type="text" ref="username" placeholder="Username" className="form-control auth-input"/>
-            <Label hidden>Login</Label>
             <input type="text" ref="login" placeholder="Login" className="form-control auth-input"/>
-            <Label hidden>Password</Label>
             <input type="password" ref="password" placeholder="Password" className="form-control auth-input"/>
             <Button type="submit" className="auth-button">Sign up</Button>
           </form>
