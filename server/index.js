15163c7938df66adba6e506c4625c0a24532ad8e
deniso95 <denis.olek@gmail.com>
2016-11-09 13:58:17 +0100

    Added passport-local to authenticate user login (connected to sequelize). Cookie-based authentication, added tmp view directory to improve backend workflows. && included morgan logger

--- server/index.js
+++ server/index.js
@@ -4,15 +4,86 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const fallback = require('express-history-api-fallback');
 
-const root = path.join(__dirname, '/../public/');
+var passport = require('passport');
+var Strategy = require('passport-local').Strategy;
+var logger = require('morgan');
+
+var Restaurant = require('./models').Restaurant;
+
+passport.use(new Strategy(
+  function(username, password, done) {
+    Restaurant.find({ where: { login: username } }).then(function(user) {
+      if (!user) {
+        return done(null, false, { message: 'Incorrect email address.' });
+      }
+      if (!user.validPassword(password)) {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+      return done(null, user);
+    });
+  }
+));
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  Restaurant.find({where: {id: id}}).then(function(user) {
+    done(null, user);
+  }).error(function(err) {
+    done(err, null);
+  });
+});
+
+// Configure view engine to render EJS templates.
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(logger('dev'));
+app.use(require('cookie-parser')());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(require('express-session')({ secret: 'supersecret', resave: false, saveUninitialized: false }));
+
+// const root = path.join(__dirname, '/../public/');
 
 var foods = require('./routes/foods');
 var restaurants = require('./routes/restaurants');
 
-app.use(express.static(root));
-app.use(fallback('index.html', {root: root}));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+// app.use(express.static(root));
+// app.use(fallback('index.html', {root: root}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Define routes.
+app.get('/',
+  function(req, res) {
+    console.log('IFAUTH: ', req.isAuthenticated());
+    res.render('home', { user: req.user });
+  });
+
+app.get('/login',
+  function(req, res) {
+    res.render('login');
+  });
+
+app.post('/login',
+  passport.authenticate('local', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/');
+  });
+
+app.get('/logout',
+  function(req, res) {
+    req.logout();
+    res.redirect('/');
+  });
+
+app.get('/profile',
+  require('connect-ensure-login').ensureLoggedIn(),
+  function(req, res) {
+    res.render('profile', { user: req.user });
+  });
 
 app.use('/api/foods', foods);
 app.use('/api/restaurants', restaurants);
--- server/models/restaurant.js
+++ server/models/restaurant.js
@@ -1,5 +1,8 @@
 'use strict';
-module.exports = function(sequelize, DataTypes) {
+
+var bcrypt = require('bcryptjs');
+
+module.exports = function (sequelize, DataTypes) {
   var Restaurant = sequelize.define('Restaurant', {
     rest_name: DataTypes.STRING,
     address: DataTypes.STRING,
@@ -8,11 +11,29 @@ module.exports = function(sequelize, DataTypes) {
     avatar: DataTypes.STRING,
     description: DataTypes.TEXT
   }, {
-    classMethods: {
-      associate: function(models) {
-        // associations can be defined here
+    hooks: {
+      beforeCreate: function (user, options, next) {
+        bcrypt.genSalt(10, function (err, salt) {
+          bcrypt.hash(user.password, salt, function (err, hash) {
+            user.password = hash;
+            next(null, user);
+          });
+        });
       }
-    }
+    },
+
+    instanceMethods: {
+      validPassword: function (password) {
+        // return bcrypt.compareSync(password, this.password);
+        if (password == this.password) {
+          return true;
+        } else {
+          return false;
+        }
+      }
+    },
+    freezeTableName: true,
+    timestamps: false
   });
   return Restaurant;
 };
