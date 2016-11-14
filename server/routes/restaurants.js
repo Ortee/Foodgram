c4816748ced77ac9cc71e96aab886ee086372564
deniso95 <denis.olek@gmail.com>
2016-11-14 15:48:40 +0100

    new restaurant api endpoints

--- server/routes/restaurants.js
+++ server/routes/restaurants.js
@@ -10,27 +10,54 @@ const db = pgp(process.env[config.use_env_variable]);
 var Restaurant = require('../class/restaurant');
 
 
-// Get all restaurants (tmp request for development)
+// Get all restaurants
 router.get('/', function(req, res, next) {
+  console.log('leci');
   db.any(
     'SELECT id, rest_name, address, login, password, avatar, description, created_at, updated_at FROM "Restaurant" ORDER BY created_at DESC')
     .then(function(data) {
       res.setHeader('Content-Type', 'application/json');
-      Restaurant = data.map((elem) => new Restaurant(
-        elem.id,
+      var tmpRestaurant = data.map((elem) => new Restaurant(
+        0,
         elem.rest_name,
         elem.address,
-        elem.login,
-        elem.password,
+        0,
+        0,
         elem.avatar,
         elem.description,
-        elem.created_at,
-        elem.updated_at));
-      res.json(Restaurant);
+        0,
+        0));
+      res.json(tmpRestaurant);
     })
     .catch(function(error) {
       res.status(404).send();
     });
 });
+
+// Get single restaurant
+router.get('/:login', function(req, res, next) {
+  var _login = req.params.login;
+  console.log('leci2');
+  db.any(
+    'SELECT id, rest_name, address, login, password, avatar, description, created_at, updated_at FROM "Restaurant" WHERE login = $1', _login)
+    .then(function(data) {
+      res.setHeader('Content-Type', 'application/json');
+      var tmpRestaurant = data.map((elem) => new Restaurant(
+        0,
+        elem.rest_name,
+        elem.address,
+        0,
+        0,
+        elem.avatar,
+        elem.description,
+        0,
+        0));
+      res.json(tmpRestaurant);
+    })
+    .catch(function(error) {
+      res.status(404).send();
+    });
+});
+
 
 module.exports = router;
