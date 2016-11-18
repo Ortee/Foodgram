82459be50fafaef26849e27c8e289de091a2e493
Mateusz <kontakt@mateuszoracz.pl>
2016-11-18 16:04:09 +0100

    user update (rest_name, description, avatar, address) in one sequlize query

--- server/routes/restaurants.js
+++ server/routes/restaurants.js
@@ -5,14 +5,10 @@ const pgp = require('pg-promise')();
 const env = process.env.NODE_ENV || 'development';
 const config = require(path.join(__dirname, '/../config/config.json'))[env];
 const db = pgp(process.env[config.use_env_variable]);
+const models = require('../models');
 
 //classes
 var Restaurant = require('../class/restaurant');
-
-function getTimestamp() {
-  return new Date(new Date().getTime() + (new Date().getTimezoneOffset()
-    * 60000) + (3600000 * 2));
-}
 
 // Get all restaurants
 router.get('/', function(req, res, next) {
@@ -61,70 +57,20 @@ router.get('/:login', function(req, res, next) {
     });
 });
 
-// Update restaurant rest_name
-router.put('/rest_name', function(req, res, next) {
+// Update restaurant
+router.put('/update', function(req, res, next) {
   req.accepts('application/json');
-  var _id = req.body[0].login;
-  db.query('UPDATE "Restaurant" SET "rest_name" = $2, "updated_at" = $3 WHERE "login" = $1',
-    [
-      _id,
-      req.body[0].rest_name,
-      getTimestamp()
-    ])
-    .then(function() {
-      res.status(201).send();
-    })
-    .catch(function(error) {
-      res.status(404).send();
-    });
-});
-
-// Update restaurant address
-router.put('/address', function(req, res, next) {
-  req.accepts('application/json');
-  var _id = req.body[0].login;
-  db.query('UPDATE "Restaurant" SET "address" = $2, "updated_at" = $3 WHERE "login" = $1',
-    [
-      _id,
-      req.body[0].address,
-      getTimestamp()
-    ])
-    .then(function() {
-      res.status(201).send();
-    })
-    .catch(function(error) {
-      res.status(404).send();
-    });
-});
-
-// Update restaurant avatar
-router.put('/avatar', function(req, res, next) {
-  req.accepts('application/json');
-  var _id = req.body[0].login;
-  db.query('UPDATE "Restaurant" SET "avatar" = $2, "updated_at" = $3 WHERE "login" = $1',
-    [
-      _id,
-      req.body[0].avatar,
-      getTimestamp()
-    ])
-    .then(function() {
-      res.status(201).send();
-    })
-    .catch(function(error) {
-      res.status(404).send();
-    });
-});
-
-// Update restaurant description
-router.put('/description', function(req, res, next) {
-  req.accepts('application/json');
-  var _id = req.body[0].login;
-  db.query('UPDATE "Restaurant" SET "description" = $2, "updated_at" = $3 WHERE "login" = $1',
-    [
-      _id,
-      req.body[0].description,
-      getTimestamp()
-    ])
+  var _login = req.body[0].login;
+  var update = {};
+  if (req.body[0].rest_name !== null) Object.assign(update, {rest_name: req.body[0].rest_name});
+  if (req.body[0].address !== null) Object.assign(update, {address: req.body[0].address});
+  if (req.body[0].avatar !== null) Object.assign(update, {avatar: req.body[0].avatar});
+  if (req.body[0].description !== null) Object.assign(update, {description: req.body[0].description});
+  models.Restaurant.update(update, {
+    where: {
+      login: _login
+    }
+  })
     .then(function() {
       res.status(201).send();
     })
