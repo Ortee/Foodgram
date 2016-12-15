a747e09fbaff1fa1867d5defd04683a2f7868cc8
Denis Olek <denis.olek@gmail.com>
2016-12-15 01:22:13 +0100

    created tests container && removed store and unnecessary index,webpack.config

--- index.js
+++
@@ -1,9 +0,0 @@
-const app = require('./server');
-const PORT = process.env.PORT || 3000;
-const logger = require('winston');
-
-app.listen(PORT, function () {
-  logger.info(`SERVER Listening on ${PORT}`);
-});
-
-module.exports = app;
