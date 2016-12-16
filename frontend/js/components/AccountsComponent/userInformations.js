62984e8e84d9dc0139da7cadcc08680fdc3d7a52
Mateusz <kontakt@mateuszoracz.pl>
2016-12-16 02:15:56 +0100

    Avatar == false => default.png

--- frontend/js/components/AccountsComponent/userInformations.js
+++ frontend/js/components/AccountsComponent/userInformations.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Col } from 'reactstrap';
+import config from '../../config';
 import './userInformations.scss';
 
 class UserInformations extends Component {
@@ -13,7 +14,7 @@ class UserInformations extends Component {
           <img  className="img-thumbnail"
                 height="75px"
                 width="75px"
-                src={this.props.user.avatar}/>
+                src={this.props.user.avatar !== false ? config.avatar + this.props.user.login + '.png' : config.avatar + 'default.png'}/>
         </Col>
         <Col xs={{size: 12}} md={{size: 8}} id="username">
           <span>{this.props.user.rest_name}</span>
