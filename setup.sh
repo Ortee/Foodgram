d4e7ca7eb9269eb8a31a461525aa0a59517c2ba8
Mateusz <kontakt@mateuszoracz.pl>
2016-12-19 19:22:31 +0100

    fix setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,2 @@
 docker exec -it $(docker ps | grep frontend | awk '{ print $1 }') /bin/sh -c 'npm rebuild node-sass'
 docker restart $(docker ps | grep frontend | awk '{ print $1 }')
-docker-compose up
